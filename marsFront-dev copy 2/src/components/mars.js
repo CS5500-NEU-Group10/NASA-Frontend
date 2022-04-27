560fc553ba4b4d46cc6f99bf7fda7b310cdf17a1
yianan261 <ychen151@nyit.edu>
2022-04-26 19:55:17 -0700

    updated mars.js

--- marsFront-dev copy 2/src/components/mars.js
+++ marsFront-dev copy 2/src/components/mars.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useLoader, useFrame } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import texture from "./8k_mars4.jpg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Mars() {
   const myMesh = React.useRef();
@@ -12,12 +12,7 @@ export default function Mars() {
   const colorMap = useLoader(TextureLoader, texture);
 
   return (
-    <mesh
-      ref={myMesh}
-      onClick={() => (
-        <Link to="/Home" style={{ textDecoration: "none" }}></Link>
-      )}
-    >
+    <mesh ref={myMesh} onClick={() => (window.location.href = "/Home")}>
       <sphereBufferGeometry args={[2, 32, 32]} attach="geometry" />
       <meshNormalMaterial attach="material" />
       <meshStandardMaterial map={colorMap} />
